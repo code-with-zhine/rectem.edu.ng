fbd2f53a84d91e181b5ecb85b47d981f13cba25f
code-with-zhine <132347598+code-with-zhine@users.noreply.github.com>
2023-09-18 16:03:32 +0100

    Update page.js

--- src/app/schools/engineering/page.js
+++ src/app/schools/engineering/page.js
@@ -25,7 +25,7 @@ export default function Home() {
             GO HOME
           </Link>
           <Link
-            href="#others"
+            href="/#others"
             className="md:hidden flex text-xs items-center text-[#003DA5] font-semibold"
           >
             <svg
