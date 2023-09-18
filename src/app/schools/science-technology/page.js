1fc5e55e9ff799e55368a6128b7340b53ebc2b1b
code-with-zhine <132347598+code-with-zhine@users.noreply.github.com>
2023-09-18 15:36:42 +0100

    Updating UI

--- src/app/schools/science-technology/page.js
+++ src/app/schools/science-technology/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <section className="px-5 md:px-10 py-10">
-        <div className="py-4">
+        <div className="py-4 flex justify-between">
           <Link
             href="/"
             className="flex text-xs items-center text-[#003DA5] font-semibold"
@@ -23,6 +23,28 @@ export default function Home() {
               />
             </svg>
             GO HOME
+          </Link>
+          <Link
+            href="#others"
+            className="md:hidden flex text-xs items-center text-[#003DA5] font-semibold"
+          >
+            <svg
+              className="mx-2"
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M17.29 5.71a.996.996 0 0 0-1.41 0L12 9.58L8.11 5.7A.996.996 0 1 0 6.7 7.11l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59a.984.984 0 0 0 0-1.4z"
+              />
+              <path
+                fill="currentColor"
+                d="M17.29 12.3a.996.996 0 0 0-1.41 0L12 16.17l-3.88-3.88a.996.996 0 1 0-1.41 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59a.993.993 0 0 0-.01-1.4z"
+              />
+            </svg>
+            DEPARTMENTS
           </Link>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
@@ -105,7 +127,7 @@ export default function Home() {
               </ul>
             </div>
           </div>
-          <div className="sm:col-span-1 space-y-4">
+          <div id="others" className="sm:col-span-1 space-y-4">
             <div>
               <hr className="border-[#f1ab00]" />
               <h4
